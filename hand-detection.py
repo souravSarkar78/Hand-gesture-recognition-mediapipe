ad188cab6ae562be428073b5c188e5f86893801d
souravSarkar78 <ss830897@gmail.com>
2021-06-28 01:11:32 +0530

    Removed unused code

--- hand-detection.py
+++ hand-detection.py
@@ -11,40 +11,6 @@ mpDraw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 
 
-thumb_tip = 4
-
-tips = [8, 12, 16, 20]
-
-rock = [1, 0, 0, 1]
-victory = [1, 1, 0, 0]
-closed = [0, 0, 0, 0]
-opened = [1, 1, 1, 1]
-
-def gesture(landmarks):
-    if landmarks:
-        tip_state = []
-        for i in (tips):
-            if landmarks[i][1] < landmarks [i-2][1]:
-                tip_state.append(1)
-            else:
-                tip_state.append(0)
-
-        if tip_state == rock:
-            print("rock")
-        elif tip_state == victory:
-            print("victory")
-        
-        elif tip_state == closed:
-            if landmarks[thumb_tip][1] < landmarks[tips[0]][1]:
-                print("ok")
-            elif landmarks[thumb_tip][1] > landmarks[tips[3]][1]:
-                print("Not Ok")
-            else:
-                print("closed")
-
-        else:
-            print("nothing matched")
-
 while True:
     _, frame = cap.read()
 
@@ -70,11 +36,8 @@ while True:
             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
 
 
-            # print(landmarks)
-        # g = gesture(landmarks)
-        
+            print(landmarks)
 
-    # print(landmarks)
     cv2.imshow("Output", frame) 
 
     if cv2.waitKey(1) == ord('q'):
